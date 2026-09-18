fd88a7d274981a3bf82b0e5400a6cd52bc4e12bc
wrehe0 <olgaklukina856@mail.ru>
2026-09-18 08:31:48 +0000

    ffff

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,4 @@ fn main() {
     println!("Hello from Rust in Docker! 🦀");
     // Принудительный сброс буфера вывода
     io::stdout().flush().unwrap();
-    // Небольшая задержка для гарантии вывода в контейнере
-    std::thread::sleep(std::time::Duration::from_millis(200));
 }
